66d2a0a62d7af2063b2431ecacdb09b990e73ae2
Erin H <e.hatherell@gmail.com>
2022-02-13 14:35:50 +1100

    Need to fill in the working bits on server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const path = require("path");
 const database = require("./Develop/db/db")
 
-// Creating the server with Express and creating a listening port
+// Creating the server with Express and setting the port
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -16,4 +16,15 @@ app.use(express.static('public'));
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+// Server listening to port
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
 
