963a8bb3c56989d836bbcd9724cc74ea8ec8c5d1
Erin H <e.hatherell@gmail.com>
2022-02-13 13:34:30 +1100

    Adding to server.js

--- server.js
+++ server.js
@@ -6,5 +6,14 @@ const fs = require("fs");
 const path = require("path");
 const database = require("./Develop/db/db")
 
+// Creating the server with Express and creating a listening port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 // Access to public directory
 app.use(express.static('public'));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
