1ee8bbc20fde4caa21551f9061639d8b36dcdd0b
Erin H <e.hatherell@gmail.com>
2022-02-13 14:49:36 +1100

    Savepoint before attempting delete function

--- server.js
+++ server.js
@@ -15,8 +15,50 @@ app.use(express.static('public'));
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
+// Setting up page routes
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+// GET notes
+app.get("/api/notes", function (err, res) {
+    try {
+        createNoteData = fs.readFileSync("/Develop/db/db.json", "utf8");
+        console.log("Successful server contact!");
+        createNoteData = JSON.parse(createNoteData);
+    } catch (err) {
+        console.log("\n error (catch err app.get):");
+        console.log(err);
+    }
+    res.json(createNoteData);
+});
+
+// POST notes
+app.post("/api/notes", function (req, res) {
+    try {
+        createNoteData = fs.readFileSync("./Develop/db/db.json", "utf8");
+        console.log(createNoteData);
+        createNoteData = JSON.parse(createNoteData);
+        req.body.id = createNoteData.length;
+        createNoteData.push(req.body);
+        createNoteData = JSON.stringify(createNoteData);
+        fs.writeFile("./db/db.json", createNoteData, "utf8", function (err) {
+            if (err) throw err;
+        });
+  
+        res.json(JSON.parse(createNoteData));
+    } catch (err) {
+        throw err;
+        console.error(err);
+    }
+});
+  
 
 
 
